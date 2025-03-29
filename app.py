4de9abe07ef35613f9a8141065f374d90f121d37
Braullio Marques <160293002+braulliomarques@users.noreply.github.com>
2025-03-29 17:18:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -327,4 +327,4 @@ def delete_all_keys():
         return jsonify({'success': False, 'message': f'Erro ao remover todas as chaves: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    app.run(host='0.0.0.0', debug=True, port=5000) 
